eab7518a378c2d6817c1a23d2f995359166d41ae
Abraham De Alba <adealba@csumb.edu>
2017-09-12 05:01:53 +0000

    CST336Lab2

---
+++ 777/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 
+            777 Slot Machine 
+        </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            
+            <?php
+                include 'inc/functions.php';
+                play();
+            ?>
+            
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+            
+        </div>
+    </body>
+    <footer>
+        <hr>
+        CST 336. 2017&copy;DE ALBA <br/>
+        Disclaimer: MY FIRST PHP GAME. <br/>
+        For academic purposes only.
+    </footer>
+</html>
