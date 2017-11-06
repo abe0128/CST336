23c24cafd01cc0356d2b8f0f8e75a284c53b399a
Abraham De Alba <adealba@csumb.edu>
2017-11-06 22:09:55 +0000

    Heroku DB

--- Lab6/database.php
+++ Lab6/database.php
@@ -2,9 +2,16 @@
 
 function getDatabaseConnection()
 {
+    /*
     $host = "localhost";
     $username = "root";
     $password = "cst336";
+    $dbname="tech_devices_app";
+    */
+    
+    $host = getenv("CLEARDB_DATABASE_URL");
+    $username = "b27252d3fdf511";
+    $password = "0f2a27bd";
     $dbname="tech_devices_app";
 
 // Create connection
