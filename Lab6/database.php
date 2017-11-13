d91530f865a64767a004530eb739f300221601a5
Abraham De Alba <adealba@csumb.edu>
2017-11-13 21:48:58 +0000

    Lab7

--- Lab6/database.php
+++ Lab6/database.php
@@ -2,15 +2,8 @@
 
 function getDatabaseConnection()
 {
-    /*
-    $host = "localhost";
-    $username = "root";
-    $password = "cst336";
-    $dbname="tech_devices_app";
-    */
     
     $host = "us-cdbr-iron-east-05.cleardb.net";
-    //$host = "mysql://b27252d3fdf511:0f2a27bd@us-cdbr-iron-east-05.cleardb.net/heroku_802607ade1b72d2?reconnect=true"
     $username = "b27252d3fdf511";
     $password = "0f2a27bd";
     $dbname= "heroku_802607ade1b72d2";
